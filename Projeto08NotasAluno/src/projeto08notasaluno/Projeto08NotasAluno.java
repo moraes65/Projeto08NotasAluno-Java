8fbe7b707a51c43aea2c4b4d062cdb093ee069d8
moraes65 <moraes.jlcm@gmail.com>
2019-08-19 20:31:41 -0300

    init Projeto08NotasAluno

---
+++ Projeto08NotasAluno/src/projeto08notasaluno/Projeto08NotasAluno.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto08notasaluno;
+
+/**
+ *
+ * @author aluno
+ */
+public class Projeto08NotasAluno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        Solicite ao usuario que informe as
+        tres Notas (de 0 a 10) de um aluno, e calcule a 
+        média e teste as condicoes:
+        1) se a média for menor ou igual a 5.0 (Reprovado)
+        2) se a média for menor ou igual a 7.0 (Aprovado)
+        3) se a media for menor ou igual a 9.0 (Muito Bom)
+        4) se a média for maior do que 9.0 (Excelente)
+        */
+        
+    }
+    
+}
